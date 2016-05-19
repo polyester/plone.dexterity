9e5c22ec72cdc9bb54f861c53c6681d3fad72431
esteele <eric@esteele.net>
2016-05-19 14:43:26 +0200

    Disable deserialize while I figure out import issues

--- plone/dexterity/serialize/content.py
+++ plone/dexterity/serialize/content.py
@@ -12,9 +12,6 @@ from plone.jsonserializer.serializer.converters import json_compatible
 from plone.supermodel.interfaces import READ_PERMISSIONS_KEY
 from plone.supermodel.interfaces import WRITE_PERMISSIONS_KEY
 from plone.supermodel.utils import mergedTaggedValueDict
-from z3c.form.interfaces import IDataManager
-from z3c.form.interfaces import IManagerValidator
-from zExceptions import BadRequest
 from zope.component import adapter
 from zope.component import ComponentLookupError
 from zope.component import getMultiAdapter
@@ -102,106 +99,109 @@ class SerializeFolderToJson(SerializeToJson):
         ]
         return result
 
+# from z3c.form.interfaces import IDataManager
+# from z3c.form.interfaces import IManagerValidator
+# from zExceptions import BadRequest
 
-@implementer(IDeserializeFromJson)
-@adapter(IDexterityContent, Interface)
-class DeserializeFromJson(object):
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
+# @implementer(IDeserializeFromJson)
+# @adapter(IDexterityContent, Interface)
+# class DeserializeFromJson(object):
+#     def __init__(self, context, request):
+#         self.context = context
+#         self.request = request
 
-        self.sm = zopepolicy.ZopeSecurityPolicy()
+#         self.sm = zopepolicy.ZopeSecurityPolicy()
 
-        self.permission_cache = {}
+#         self.permission_cache = {}
 
-    def __call__(self, validate_all=False):
-        data = json_body(self.request)
+#     def __call__(self, validate_all=False):
+#         data = json_body(self.request)
 
-        modified = False
-        schema_data = {}
-        errors = []
+#         modified = False
+#         schema_data = {}
+#         errors = []
 
-        for schema in iterSchemata(self.context):
-            write_permissions = mergedTaggedValueDict(
-                schema, WRITE_PERMISSIONS_KEY)
+#         for schema in iterSchemata(self.context):
+#             write_permissions = mergedTaggedValueDict(
+#                 schema, WRITE_PERMISSIONS_KEY)
 
-            for name, field in getFields(schema).items():
+#             for name, field in getFields(schema).items():
 
-                field_data = schema_data.setdefault(schema, {})
+#                 field_data = schema_data.setdefault(schema, {})
 
-                if field.readonly:
-                    continue
+#                 if field.readonly:
+#                     continue
 
-                if name in data:
-                    dm = queryMultiAdapter((self.context, field), IDataManager)
-                    if not dm.canWrite():
-                        continue
+#                 if name in data:
+#                     dm = queryMultiAdapter((self.context, field), IDataManager)
+#                     if not dm.canWrite():
+#                         continue
 
-                    if not self.check_permission(write_permissions.get(name)):
-                        continue
+#                     if not self.check_permission(write_permissions.get(name)):
+#                         continue
 
-                    # Deserialize to field value
-                    deserializer = queryMultiAdapter(
-                        (field, self.context, self.request),
-                        IFieldDeserializer)
-                    if deserializer is None:
-                        continue
+#                     # Deserialize to field value
+#                     deserializer = queryMultiAdapter(
+#                         (field, self.context, self.request),
+#                         IFieldDeserializer)
+#                     if deserializer is None:
+#                         continue
 
-                    try:
-                        value = deserializer(data[name])
-                    except ValueError as e:
-                        errors.append({
-                            'message': e.message, 'field': name, 'error': e})
-                    except ValidationError as e:
-                        errors.append({
-                            'message': e.doc(), 'field': name, 'error': e})
-                    else:
-                        field_data[name] = value
-                        if value != dm.get():
-                            dm.set(value)
-                            modified = True
+#                     try:
+#                         value = deserializer(data[name])
+#                     except ValueError as e:
+#                         errors.append({
+#                             'message': e.message, 'field': name, 'error': e})
+#                     except ValidationError as e:
+#                         errors.append({
+#                             'message': e.doc(), 'field': name, 'error': e})
+#                     else:
+#                         field_data[name] = value
+#                         if value != dm.get():
+#                             dm.set(value)
+#                             modified = True
 
-                elif validate_all:
-                    # Never validate the changeNote of p.a.versioningbehavior
-                    # The Versionable adapter always returns an empty string
-                    # which is the wrong type. Should be unicode and should be
-                    # fixed in p.a.versioningbehavior
-                    if name == 'changeNote':
-                        continue
-                    dm = queryMultiAdapter((self.context, field), IDataManager)
-                    bound = field.bind(self.context)
-                    try:
-                        bound.validate(dm.get())
-                    except ValidationError as e:
-                        errors.append({
-                            'message': e.doc(), 'field': name, 'error': e})
+#                 elif validate_all:
+#                     # Never validate the changeNote of p.a.versioningbehavior
+#                     # The Versionable adapter always returns an empty string
+#                     # which is the wrong type. Should be unicode and should be
+#                     # fixed in p.a.versioningbehavior
+#                     if name == 'changeNote':
+#                         continue
+#                     dm = queryMultiAdapter((self.context, field), IDataManager)
+#                     bound = field.bind(self.context)
+#                     try:
+#                         bound.validate(dm.get())
+#                     except ValidationError as e:
+#                         errors.append({
+#                             'message': e.doc(), 'field': name, 'error': e})
 
-        # Validate schemata
-        for schema, field_data in schema_data.items():
-            validator = queryMultiAdapter(
-                (self.context, self.request, None, schema, None),
-                IManagerValidator)
-            for error in validator.validate(field_data):
-                errors.append({'error': error, 'message': error.message})
+#         # Validate schemata
+#         for schema, field_data in schema_data.items():
+#             validator = queryMultiAdapter(
+#                 (self.context, self.request, None, schema, None),
+#                 IManagerValidator)
+#             for error in validator.validate(field_data):
+#                 errors.append({'error': error, 'message': error.message})
 
-        if errors:
-            raise BadRequest(errors)
+#         if errors:
+#             raise BadRequest(errors)
 
-        if modified:
-            notify(ObjectModifiedEvent(self.context))
+#         if modified:
+#             notify(ObjectModifiedEvent(self.context))
 
-        return self.context
+#         return self.context
 
-    def check_permission(self, permission_name):
-        if permission_name is None:
-            return True
+#     def check_permission(self, permission_name):
+#         if permission_name is None:
+#             return True
 
-        if permission_name not in self.permission_cache:
-            permission = queryUtility(IPermission,
-                                      name=permission_name)
-            if permission is None:
-                self.permission_cache[permission_name] = True
-            else:
-                self.permission_cache[permission_name] = bool(
-                    self.sm.checkPermission(permission.title, self.context))
-        return self.permission_cache[permission_name]
+#         if permission_name not in self.permission_cache:
+#             permission = queryUtility(IPermission,
+#                                       name=permission_name)
+#             if permission is None:
+#                 self.permission_cache[permission_name] = True
+#             else:
+#                 self.permission_cache[permission_name] = bool(
+#                     self.sm.checkPermission(permission.title, self.context))
+#         return self.permission_cache[permission_name]
