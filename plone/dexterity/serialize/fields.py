b486cd78470c548981283b6c14146ff7c7bee4c5
esteele <eric@esteele.net>
2016-05-19 14:07:44 +0200

    Remove CMFCore, CMFPlone calls. We'll need to fix image scale lookups

--- plone/dexterity/serialize/fields.py
+++ plone/dexterity/serialize/fields.py
@@ -2,22 +2,14 @@
 from plone.dexterity.interfaces import IDexterityContent
 from plone.namedfile.interfaces import INamedFileField
 from plone.namedfile.interfaces import INamedImageField
+from plone.registry.interfaces import IRegistry
 from plone.restapi.interfaces import IFieldSerializer
 from plone.restapi.serializer.converters import json_compatible
-from Products.CMFCore.interfaces import IPropertiesTool
 from zope.component import adapter
 from zope.component import getUtility
 from zope.interface import implementer
 from zope.interface import Interface
 from zope.schema.interfaces import IField
-
-
-try:
-    from Products.CMFPlone.factory import _IMREALLYPLONE5  # noqa
-except ImportError:
-    PLONE_5 = False  # pragma: no cover
-else:
-    PLONE_5 = True  # pragma: no cover
 
 
 @adapter(IField, IDexterityContent, Interface)
@@ -51,22 +43,16 @@ class ImageFieldSerializer(DefaultFieldSerializer):
         return json_compatible(urls)
 
     def get_scale_names(self):
-        if PLONE_5:
-            from plone.registry.interfaces import IRegistry
-            registry = getUtility(IRegistry)
-            from Products.CMFPlone.interfaces import IImagingSchema
-            imaging_settings = registry.forInterface(
-                IImagingSchema,
-                prefix='plone'
-            )
-            allowed_sizes = imaging_settings.allowed_sizes
+        registry = getUtility(IRegistry)
+        # TODO: Get scale names from somewhere.
+        # from Products.CMFPlone.interfaces import IImagingSchema
+        # imaging_settings = registry.forInterface(
+        #     IImagingSchema,
+        #     prefix='plone'
+        # )
+        # allowed_sizes = imaging_settings.allowed_sizes
 
-        else:
-            ptool = getUtility(IPropertiesTool)
-            image_properties = ptool.imaging_properties
-            allowed_sizes = image_properties.getProperty('allowed_sizes')
-
-        return [size.split(' ')[0] for size in allowed_sizes]
+        # return [size.split(' ')[0] for size in allowed_sizes]
 
 
 @adapter(INamedFileField, IDexterityContent, Interface)
