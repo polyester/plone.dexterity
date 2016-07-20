770dd44ffa3c236db86d6e2108ce63069041e7b7
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-07-20 13:25:44 +0200

    No dublin core on dexterity, its managed by the behavior no need to have extra attributed on DX objects for that

--- plone/dexterity/content.py
+++ plone/dexterity/content.py
@@ -2,36 +2,27 @@
 from copy import deepcopy
 from datetime import datetime
 from persistent import Persistent
+
 from dateutil.tz import tzlocal
-from dateutil.tz import tzutc
 from plone.behavior.interfaces import IBehaviorAssignable
 from zope.annotation import IAttributeAnnotatable
 from zope.container.contained import Contained
 from zope.container.ordered import OrderedContainer
-from zope.dublincore.interfaces import IWriteZopeDublinCore
 from zope.interface import implementer
 from zope.interface.declarations import Implements
 from zope.interface.declarations import ObjectSpecificationDescriptor
 from zope.interface.declarations import getObjectSpecification
 from zope.interface.declarations import implementedBy
 from zope.schema.interfaces import IContextAwareDefaultFactory
-from zope.traversing.browser.interfaces import IAbsoluteURL
 from plone.dexterity.interfaces import IDexterityContainer
 from plone.dexterity.interfaces import IDexterityContent
 from plone.dexterity.interfaces import IDexterityItem
 from plone.dexterity.schema import SCHEMA_CACHE
-from plone.dexterity.utils import safe_str
 from plone.uuid.interfaces import IAttributeUUID
 from plone.uuid.interfaces import IUUID
 
 _marker = object()
 _zone = tzlocal()
-_utc = tzutc()
-
-# always effective
-FLOOR_DATE = datetime(*datetime.min.timetuple()[:-2], tzutc())
-# never expires
-CEILING_DATE = datetime(*datetime.max.timetuple()[:-2], tzutc())
 
 
 def _default_from_schema(context, schema, fieldname):
@@ -135,8 +126,7 @@ class FTIAwareSpecification(ObjectSpecificationDescriptor):
 @implementer(
     IDexterityContent,
     IAttributeAnnotatable,
-    IAttributeUUID,
-    IWriteZopeDublinCore,
+    IAttributeUUID
 )
 class DexterityContent(Persistent, Contained):
     """Base class for Dexterity content
@@ -147,48 +137,16 @@ class DexterityContent(Persistent, Contained):
     # portal_type is set by the add view and/or factory
     portal_type = None
 
-    title = u''
-    description = u''
-    subject = ()
-    creators = ()
-    contributors = ()
-    effective_date = None
-    expiration_date = None
-    format = 'text/html'
-    language = ''
-    rights = ''
-
     def __init__(  # noqa
             self,
-            id=None, title=_marker, subject=_marker, description=_marker,
-            contributors=_marker, effective_date=_marker,
-            expiration_date=_marker, format=_marker, language=_marker,
-            rights=_marker, **kwargs):
+            id=None,
+            **kwargs):
 
         if id is not None:
             self.id = id
         now = datetime.now(tz=_zone)
         self.creation_date = now
         self.modification_date = now
-
-        if title is not _marker:
-            self.setTitle(title)
-        if subject is not _marker:
-            self.setSubject(subject)
-        if description is not _marker:
-            self.setDescription(description)
-        if contributors is not _marker:
-            self.setContributors(contributors)
-        if effective_date is not _marker:
-            self.setEffectiveDate(effective_date)
-        if expiration_date is not _marker:
-            self.setExpirationDate(expiration_date)
-        if format is not _marker:
-            self.setFormat(format)
-        if language is not _marker:
-            self.setLanguage(language)
-        if rights is not _marker:
-            self.setRights(rights)
 
         for (k, v) in kwargs.items():
             setattr(self, k, v)
@@ -248,237 +206,6 @@ class DexterityContent(Persistent, Contained):
         """Returns the item's globally unique id."""
         return IUUID(self)
 
-    def notifyModified(self):
-        """Update creators and modification_date.
-
-        This is called from CMFCatalogAware.reindexObject.
-        """
-        self.addCreator()
-        self.setModificationDate()
-
-    def addCreator(self, creator=None):
-        """ Add creator to Dublin Core creators.
-        """
-        if len(self.creators) > 0:
-            # do not add creator if one is already set
-            return
-
-        if creator is None:
-            return
-
-        # call self.listCreators() to make sure self.creators exists
-        if creator and creator not in self.listCreators():
-            self.creators = self.creators + (creator, )
-
-    def setModificationDate(self, modification_date=None):
-        """ Set the date when the resource was last modified.
-
-        When called without an argument, sets the date to now.
-        """
-        if modification_date is None:
-            self.modification_date = datetime.now(tz=_zone)
-        else:
-            self.modification_date = modification_date
-
-    # IMinimalDublinCore
-
-    def Title(self):
-        return self.title or ''
-
-    def Description(self):
-        return self.description or ''
-
-    def Type(self):
-        ti = self.getTypeInfo()
-        return ti is not None and ti.Title() or 'Unknown'
-
-    # IDublinCore
-
-    def listCreators(self):
-        # List Dublin Core Creator elements - resource authors.
-        if self.creators is None:
-            return ()
-        return self.creators
-
-    def Creator(self):
-        # Dublin Core Creator element - resource author.
-        creators = self.listCreators()
-        return creators and creators[0] or ''
-
-    def Subject(self):
-        # Dublin Core Subject element - resource keywords.
-        if self.subject is None:
-            return ()
-        return self.subject
-
-    def Publisher(self):
-        # Dublin Core Publisher element - resource publisher.
-        return 'No publisher'
-
-    def listContributors(self):
-        # Dublin Core Contributor elements - resource collaborators.
-        return self.contributors
-
-    def Contributors(self):
-        # Deprecated alias of listContributors.
-        return self.listContributors()
-
-    def Date(self, zone=None):
-        # Dublin Core Date element - default date.
-        if zone is None:
-            zone = _zone
-        # Return effective_date if set, modification date otherwise
-        date = getattr(self, 'effective_date', None)
-        if date is None:
-            date = self.modified()
-        try:
-            return date.astimezone(zone).isoformat()
-        except (TypeError, ValueError):
-            return date.isoformat()
-
-    def CreationDate(self, zone=None):
-        # Dublin Core Date element - date resource created.
-        if zone is None:
-            zone = _zone
-        # return unknown if never set properly
-        if self.creation_date:
-            try:
-                return self.creation_date.astimezone(zone).isoformat()
-            except (TypeError, ValueError):
-                return self.creation_date.isoformat()
-        else:
-            return 'Unknown'
-
-    def EffectiveDate(self, zone=None):
-        # Dublin Core Date element - date resource becomes effective.
-        if zone is None:
-            zone = _zone
-        if getattr(self, 'effective_date', None):
-            try:
-                return self.effective_date.astimezone(zone).isoformat()
-            except (TypeError, ValueError):
-                return self.effective_date.isoformat()
-        else:
-            return None
-
-    def ExpirationDate(self, zone=None):
-        # Dublin Core Date element - date resource expires.
-        if zone is None:
-            zone = _zone
-        if getattr(self, 'expiration_date', None):
-            try:
-                return self.expiration_date.astimezone(zone).isoformat()
-            except (TypeError, ValueError):
-                return self.expiration_date.isoformat()
-        else:
-            return None
-
-    def ModificationDate(self, zone=None):
-        # Dublin Core Date element - date resource last modified.
-        if zone is None:
-            zone = _zone
-        try:
-            return self.modified().astimezone(zone).isoformat()
-        except (TypeError, ValueError):
-            return self.modified().isoformat()
-
-    def Identifier(self):
-        # Dublin Core Identifier element - resource ID.
-        return str(IAbsoluteURL(self, IUUID(self)))
-
-    def Language(self):
-        # Dublin Core Language element - resource language.
-        return self.language
-
-    def Rights(self):
-        # Dublin Core Rights element - resource copyright.
-        return self.rights
-
-    # ICatalogableDublinCore
-
-    def created(self):
-        # Dublin Core Date element - date resource created.
-        # allow for non-existent creation_date, existed always
-        date = getattr(self, 'creation_date', None)
-        return date is None and FLOOR_DATE or date
-
-    def effective(self):
-        # Dublin Core Date element - date resource becomes effective.
-        date = getattr(self, 'effective_date', _marker)
-        if date is _marker:
-            date = getattr(self, 'creation_date', None)
-        return date is None and FLOOR_DATE or date
-
-    def expires(self):
-        # Dublin Core Date element - date resource expires.
-        date = getattr(self, 'expiration_date', None)
-        return date is None and CEILING_DATE or date
-
-    def modified(self):
-        # Dublin Core Date element - date resource last modified.
-        date = self.modification_date
-        if date is None:
-            # Upgrade.
-            date = datetime.fromtimestamp(self._p_mtime, tz=_zone)
-            self.modification_date = date
-        return date
-
-    def isEffective(self, date):
-        # Is the date within the resource's effective range?
-        pastEffective = (
-            self.effective_date is None or self.effective_date <= date)
-        beforeExpiration = (
-            self.expiration_date is None or self.expiration_date >= date)
-        return pastEffective and beforeExpiration
-
-    # IMutableDublinCore
-
-    def setTitle(self, title):
-        # Set Dublin Core Title element - resource name.
-        self.title = safe_str(title)
-
-    def setDescription(self, description):
-        # Set Dublin Core Description element - resource summary.
-        self.description = safe_str(description)
-
-    def setCreators(self, creators):
-        # Set Dublin Core Creator elements - resource authors.
-        if isinstance(creators, str):
-            creators = [creators]
-        self.creators = tuple(safe_str(c.strip()) for c in creators)
-
-    def setSubject(self, subject):
-        # Set Dublin Core Subject element - resource keywords.
-        if isinstance(subject, str):
-            subject = [subject]
-        self.subject = tuple(safe_str(s.strip()) for s in subject)
-
-    def setContributors(self, contributors):
-        # Set Dublin Core Contributor elements - resource collaborators.
-        if isinstance(contributors, str):
-            contributors = contributors.split(';')
-        self.contributors = tuple(
-                safe_str(c.strip()) for c in contributors)
-
-    def setEffectiveDate(self, effective_date):
-        # Set Dublin Core Date element - date resource becomes effective.
-        self.effective_date = effective_date
-
-    def setExpirationDate(self, expiration_date):
-        # Set Dublin Core Date element - date resource expires.
-        self.expiration_date = expiration_date
-
-    def setFormat(self, format):
-        # Set Dublin Core Format element - resource format.
-        self.format = format
-
-    def setLanguage(self, language):
-        # Set Dublin Core Language element - resource language.
-        self.language = language
-
-    def setRights(self, rights):
-        # Set Dublin Core Rights element - resource copyright.
-        self.rights = safe_str(rights)
 
 
 @implementer(IDexterityItem)
@@ -498,12 +225,6 @@ class Container(OrderedContainer, DexterityContent):
     """
 
     __providedBy__ = FTIAwareSpecification()
-
-    # Make sure PortalFolder's accessors and mutators don't take precedence
-    Title = DexterityContent.Title
-    setTitle = DexterityContent.setTitle
-    Description = DexterityContent.Description
-    setDescription = DexterityContent.setDescription
 
     def __init__(self, id=None, **kwargs):
         OrderedContainer.__init__(self)
